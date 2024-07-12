d6a64f34ab854663dccead3c2ab575440ddff0b5
nandu1-k <121676407+nandu1-k@users.noreply.github.com>
2024-07-12 12:15:28 +0530

    n

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,23 +6,22 @@
 * License: https://bootstrapmade.com/license/
 */
 
-// (function() {
-//   "use strict";
-//   const preloader = document.querySelector('#preloader');
-//   if (preloader) {
-//     window.addEventListener('load', () => {
-//         preloader.remove();
-//     });
-//   }
+(function() {
+  "use strict";
+  const preloader = document.querySelector('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+       preloader.remove();
+   });   }
   
-//   document.addEventListener('scroll', function() {
-//     var element = document.querySelector('.navbar');
-//     if (window.scrollY > 200) {
-//         element.classList.add('scroll');
-//     } else {
-//         element.classList.remove('scroll');
-//     }
-// });
+ document.addEventListener('scroll', function() {
+     var element = document.querySelector('.navbar');
+    if (window.scrollY > 200) {
+       element.classList.add('scroll');
+    } else {
+        element.classList.remove('scroll');
+    }
+ });
 
   /**
    * Apply .scrolled class to the body as the page is scrolled down
